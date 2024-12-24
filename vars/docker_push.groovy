efb0b80a5623a4bebe04e58dee8104e230a0853b
ASAPU HARISH <141323054+harishasapu@users.noreply.github.com>
2024-12-24 17:13:30 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,14 +1,1 @@
-def call(String Project, String ImageTag, String dockerhubuser) {
-    // Use Jenkins credentials binding to securely inject the username and password
-    withCredentials([usernamePassword(credentialsId: 'docker', 
-                                      passwordVariable: 'DOCKER_PASSWORD', 
-                                      usernameVariable: 'DOCKER_USERNAME')]) {
-        // Securely login to Docker using --password-stdin
-        sh '''
-        echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-        '''
-    }
-
-    // Push the image to Docker Hub using the provided project and image tag
-    sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
-}
+ 

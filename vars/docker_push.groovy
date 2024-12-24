8c9640196803edadaac91a7f2ca31ec3c84bbdc4
ASAPU HARISH <141323054+harishasapu@users.noreply.github.com>
2024-12-24 16:52:09 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,3 +1,3 @@
-withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+withCredentials([usernamePassword(credentialsId: 'docker-cred', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
     sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
 }
